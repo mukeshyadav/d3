d0d136fb3e838aebeade0e778fe26fb0c2b4079c
Mukesh Yadav <mukeshyadav@gmail.com>
2020-07-01 20:46:04 +0530

    creating canvas and printing a circle

--- scripts/scripts-v1.js
+++ scripts/scripts-v1.js
@@ -1,1 +1,24 @@
-import * as d3 from "d3";
+function createCanvas(elemId) {
+  return d3
+    .select(elemId)
+    .append("svg")
+    .attr("width", 400)
+    .attr("height", 400);
+}
+
+function createCircle(elemSVG) {
+  return elemSVG
+    .append("circle")
+    .attr("cx", 150)
+    .attr("cy", 150)
+    .attr("r", 50)
+    .attr("fill", "red");
+}
+
+async function createChart(elemId) {
+  let svg = await createCanvas(elemId);
+  let circle = await createCircle(svg);
+  return circle;
+}
+
+createChart("#chart");
